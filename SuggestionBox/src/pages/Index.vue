535c268210b9c87da30e29920c9980703a9b6638
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2019-06-06 14:08:07 -1000

    Created a working submit button that can be disabled

--- SuggestionBox/src/pages/Index.vue
+++ SuggestionBox/src/pages/Index.vue
@@ -1,61 +1,25 @@
 <template>
   <div>
-    <br>
-    <p class = "titleSize">DROPDOWN</p>
-    <div class="q-pa-md" style="max-width: 300px">
-      <q-select  outlined v-model="DROPDOWN" :options="options" label="DropDown Subjects"/>
+    <div>
+      <q-btn @click = "save" color="primary" v-bind:disabled="enable" label="Primary"/>
     </div>
-    <br>
-
-    <p class = "titleSize">Subject</p>
-    <div class="q-pa-md" style="max-width: 300px">
-      <q-input square outlined v-model="SUBJECT" label="Subject" />
-    </div>
-    <br>
-
-    <p class = "titleSize">Description</p>
-    <div class="q-pa-md" style="max-width: 300px">
-      <q-input square outlined v-model="DESCRIPTION" label="Description" />
-    </div>
-    <br>
-    <div><q-btn @click = "save" color="primary" label="Primary"/></div>
-    <!-- to = "/index"-->
-    <p> {{ showit }} </p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'suggestion',
+  name: 'PageIndex',
   data() {
     return {
-      DROPDOWN: '',
-      SUBJECT: ' ',
-      DESCRIPTION: ' ',
-      savedDROPDOWN: ' ',
-      savedSUBJECT: ' ',
-      savedDESCRIPTION: ' ',
-      options: [
-        'Company Improvement', 'Employee Happiness', 'Other',
-      ],
+      enable: true,
+      message: 'Hello',
     };
-  },
-  methods: {
-    save() {
-      this.savedDROPDOWN = this.DROPDOWN;
-      this.savedSUBJECT = this.SUBJECT;
-      this.savedDESCRIPTION = this.DESCRIPTION;
-    },
-  },
-  computed: {
-    showit() {
-      return this.savedDROPDOWN + ' ' + this.savedSUBJECT + ' ' + this.savedDESCRIPTION;
-    },
   },
 };
 </script>
+
 <style scoped>
-  .titleSize {
-    font-size: 20px;
+  [v-cloak] {
+    display: none;
   }
 </style>
