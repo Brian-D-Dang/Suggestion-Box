d6afde3d9c255ab52b4d2fd41bdb37020f33ea09
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2019-06-06 16:14:09 -1000

    Created objects to replace the individual variables.

---
+++ SuggestionBox/src/pages/Suggestion.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <br>
+    <p class="titleSize">DROPDOWN</p>
+    <div class="q-pa-md" style="max-width: 300px">
+      <q-select outlined v-model="survey.DROPDOWN" :options="options" label="DropDown Subjects"/>
+    </div>
+    <br>
+
+    <p class="titleSize">Subject</p>
+    <div class="q-pa-md" style="max-width: 300px">
+      <q-input square outlined v-model="survey.SUBJECT" label="Subject" />
+    </div>
+    <br>
+
+    <p class="titleSize">Description</p>
+    <div class="q-pa-md" style="max-width: 300px">
+      <q-input square outlined v-model="survey.DESCRIPTION" label="Description" />
+    </div>
+    <br>
+    <div><q-btn @click="save" color="primary" label="Primary"/></div>
+    <!-- to="/index"-->
+    <p> {{ showIt }} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Suggestion',
+  data() {
+    return {
+      survey: {
+        DROPDOWN: '',
+        SUBJECT: ' ',
+        DESCRIPTION: ' ',
+      },
+      savedSurvey: {
+        savedDROPDOWN: ' ',
+        savedSUBJECT: ' ',
+        savedDESCRIPTION: ' ',
+      },
+      options: [
+        'Company Improvement', 'Employee Happiness', 'Other',
+      ],
+    };
+  },
+  methods: {
+    save() {
+      this.savedSurvey.savedDROPDOWN = this.survey.DROPDOWN;
+      this.savedSurvey.savedSUBJECT = this.survey.SUBJECT;
+      this.savedSurvey.savedDESCRIPTION = this.survey.DESCRIPTION;
+    },
+  },
+  computed: {
+    showIt() {
+      const { savedDROPDOWN, savedSUBJECT, savedDESCRIPTION } = this.savedSurvey;
+      return savedDROPDOWN + ' ' + savedSUBJECT + ' ' + savedDESCRIPTION;
+    },
+  },
+};
+</script>
+<style scoped>
+  .titleSize {
+    font-size: 20px;
+  }
+</style>
