4e3edf7436fddc7d8d028bfa3a7cb67d684863f2
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2019-06-07 14:54:22 -1000

    Created the new login screen with working inputs and button.

--- SuggestionBox/src/pages/Index.vue
+++ SuggestionBox/src/pages/Index.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <q-btn @click="save" color="primary" :disabled="enable" label="Primary"/>
+    <p class = "title-size">Email:</p>
+    <div>
+      <q-input square outlined v-model="cred.username" label="Email" />
+    </div>
+    <p class = "title-size">Password:</p>
+    <div>
+      <q-input square outlined v-model="cred.password" label="Password" />
+    </div>
+    <q-btn color="primary" :disabled="!activate" label="LOGIN"/>
+    <!-- to="/suggestion" -->
   </div>
 </template>
 
@@ -9,13 +18,20 @@ export default {
   name: 'PageIndex',
   data() {
     return {
-      enable: true,
-      message: 'Hello',
+      cred: {},
     };
+  },
+  computed: {
+    activate() {
+      const { username, password } = this.cred;
+      return username && password;
+    },
   },
 };
 </script>
 
 <style scoped>
-
+  .title-size {
+    font-size: 20px;
+  }
 </style>
