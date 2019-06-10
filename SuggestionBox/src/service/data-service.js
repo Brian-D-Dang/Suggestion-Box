6c68a2641e7af67f9a98e2599bb43ecb6b75b38f
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2019-06-10 10:37:00 -1000

    Created a new directory.

---
+++ SuggestionBox/src/service/data-service.js
@@ -0,0 +1,20 @@
+async function getSuggestionRows() {
+  return [
+    {
+      suggestionId: 1,
+      category: 'Employee Happiness',
+      subject: '',
+      text: 'I like to dance in the rain and that makes me happy',
+      date: '19-JUN-07',
+      // not the real deal
+    },
+    {
+      suggestionId: 2,
+      category: 'Employee Happiness',
+      subject: '',
+      text: 'James needs more \"happy\" pills',
+      date: '19-JUN-08',
+      // not the real deal
+    },
+  ];
+}
