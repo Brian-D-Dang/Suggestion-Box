6424b86807319d14885c930753e42259b9f1462c
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2019-06-10 17:15:51 -1000

    A for loop was made to list all of the array values.

--- SuggestionBox/src/pages/Dashboard.vue
+++ SuggestionBox/src/pages/Dashboard.vue
@@ -1,13 +1,29 @@
 <template>
   <div>
+    <!-- <p v-for="suggestion in suggestionRows" :key="suggestion.id">{{ suggestion }}</p> -->
+    <p v-for="suggestion in suggestionRows" :key="suggestion.id">{{ suggestion }}</p>
     <q-btn to="/suggestion" color="primary" label="SuggestionForm"/>
   </div>
 </template>
 
 <script>
+import dataService from 'src/service/data-service.js';
+
 export default {
   name: 'Dashboard',
+  data() {
+    return {
+      suggestionRows: null,
+    };
+  },
+  created() {
+    (async () => {
+      this.suggestionRows = dataService.savedForms;
+      console.log(this.suggestionRows);
+    })();
+  },
 };
+// use this import to display the array from data-service
 </script>
 
 <style scoped>
--- SuggestionBox/src/service/data-service.js
+++ SuggestionBox/src/service/data-service.js
@@ -1,22 +1,6 @@
 export default {
+  savedForms: [],
   async getSuggestionRows() {
-    return [
-      {
-        suggestionId: 1,
-        category: 'Employee Happiness',
-        subject: '',
-        text: 'I like to dance in the rain and that makes me happy',
-        date: '19-JUN-07',
-        // not the real deal
-      },
-      {
-        suggestionId: 2,
-        category: 'Employee Happiness',
-        subject: '',
-        text: 'James needs more "happy" pills',
-        date: '19-JUN-08',
-        // not the real deal
-      },
-    ];
+    return 'hello';
   },
 };
