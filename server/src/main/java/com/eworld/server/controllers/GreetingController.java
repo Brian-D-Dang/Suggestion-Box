d787b1448e159e6a98f2670b92891c7187c9fd90
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2019-06-20 15:30:53 -1000

    REST Web Services - Practice.

---
+++ server/src/main/java/com/eworld/server/controllers/GreetingController.java
@@ -0,0 +1,36 @@
+package com.eworld.server.controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.lang.Math;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class GreetingController {
+    private static Map<Long, Greeting> productRepo = new HashMap<>();
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    public GreetingController() {}
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public ResponseEntity<Object> createProduct(@RequestBody Greeting greeting) {
+        productRepo.put(greeting.getId(), greeting);
+        double rand = Math.random();
+        return new ResponseEntity<>(rand, HttpStatus.CREATED);
+    }
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
