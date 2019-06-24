a663895e139bfd2bedba3ba82190c99299474429
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2019-06-24 09:45:37 -1000

    Changed forbidden to created.

--- server/src/main/java/com/eworld/server/controllers/GreetingController.java
+++ server/src/main/java/com/eworld/server/controllers/GreetingController.java
@@ -26,7 +26,7 @@ public class GreetingController {
     public ResponseEntity<Object> createProduct(@RequestBody Greeting greeting) {
         productRepo.put(greeting.getId(), greeting);
         double rand = Math.random();
-        return new ResponseEntity<>(rand, HttpStatus.FORBIDDEN);
+        return new ResponseEntity<>(rand, HttpStatus.CREATED);
     }
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
