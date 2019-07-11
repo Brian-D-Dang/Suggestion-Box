6ebcf53cf2a65d4b045578f3cd97f1f293709334
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2019-07-10 14:48:21 -1000

    Added in a password checking parameter. This allows you to log in with both username and password.

--- server/src/main/java/com/eworld/server/UserServiceImpl.java
+++ server/src/main/java/com/eworld/server/UserServiceImpl.java
@@ -1,14 +1,20 @@
 package com.eworld.server;
 
+import com.eworld.server.entity.PasswordEntity;
 import com.eworld.server.entity.UserAccountEntity;
+import com.eworld.server.repository.PasswordRepository;
 import com.eworld.server.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sun.security.util.Password;
 
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PasswordRepository passwordRepository;
 
     public UserServiceImpl() {
 
@@ -20,8 +26,11 @@ public class UserServiceImpl implements UserService {
     @Override
     public int getUserAccountId(String username, String password) {
         UserAccountEntity userAccountEntity = userRepository.getUserAccountEntityByUsername(username);
-        if (userAccountEntity != null) {
-            return userAccountEntity.getUserAccountId();
+        PasswordEntity passwordEntity = passwordRepository.getPasswordEntityByPassword(password);
+        if ((userAccountEntity != null)&&(passwordEntity != null)) {
+            if (userAccountEntity.getUserAccountId() == passwordEntity.getUserAccountId()) {
+                return userAccountEntity.getUserAccountId();
+            }
         }
         return 0;
     }
