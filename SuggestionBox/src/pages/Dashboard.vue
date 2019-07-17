8ab9530d54065bf0eab939e2d2a893e9878eb21e
Brian Dang <30878065+WhaleAsauresRex@users.noreply.github.com>
2019-07-17 10:10:18 -1000

    Work in progress, but the right table is being used

--- SuggestionBox/src/pages/Dashboard.vue
+++ SuggestionBox/src/pages/Dashboard.vue
@@ -1,10 +1,5 @@
 <template>
   <div>
-    <q-card class="row justify-start">
-      <q-card-section class="bg-cyan" style="min-width:1000px">
-        "hello"
-      </q-card-section>
-      <q-card-section>
        <q-table
          style="width: 1000px; display:block;"
           class="col"
@@ -14,8 +9,30 @@
           :columns="columns"
           row-key="name"
         />
-        </q-card-section>
-    </q-card>
+<!--    <template v-slot:item="props">-->
+<!--      <div-->
+<!--        class="q-pa-xs col-xs-12 col-sm-6 col-md-4 col-lg-3 grid-style-transition"-->
+<!--        :style="props.selected ? 'transform: scale(0.95);' : ''"-->
+<!--      >-->
+<!--        <q-card :class="props.selected ? 'bg-grey-2' : ''">-->
+<!--          <q-card-section>-->
+<!--            <q-checkbox dense v-model="props.selected" :label="props.row.name" />-->
+<!--          </q-card-section>-->
+<!--          <q-separator />-->
+<!--          <q-list dense>-->
+<!--            <q-item
+v-for="col in props.cols.filter(col => col.name !== 'desc')" :key="col.name">-->
+<!--              <q-item-section>-->
+<!--                <q-item-label>{{ col.label }}</q-item-label>-->
+<!--              </q-item-section>-->
+<!--              <q-item-section side>-->
+<!--                <q-item-label caption>{{ col.value }}</q-item-label>-->
+<!--              </q-item-section>-->
+<!--            </q-item>-->
+<!--          </q-list>-->
+<!--        </q-card>-->
+<!--      </div>-->
+<!--    </template>-->
     <q-btn to="/suggestion" color="primary" label="Suggestion Form"/>
 
   </div>
@@ -31,6 +48,14 @@ export default {
     return {
       data: [],
       columns: [
+        {
+          name: 'Category',
+          required: true,
+          label: 'Testing',
+          align: 'left',
+          field: 'subject',
+          sortable: true,
+        },
         {
           name: 'Category', align: 'center', label: 'Category', field: 'category', sortable: true,
         },
